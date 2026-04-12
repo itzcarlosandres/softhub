1c81e0bfea6324ed893beb5cd14281fc07b19e39
itzcarlosandres <itzcarlosandres@gmail.com>
2026-04-12 14:46:51 -0500

    Delete test_api.php

--- test_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Test API endpoint
-require_once __DIR__ . '/vendor/autoload.php';
-
-$_GET['q'] = 'spotify';
-$_GET['filter'] = 'all';
-
-$controller = new \App\Controllers\ApiController();
-$controller->search();
